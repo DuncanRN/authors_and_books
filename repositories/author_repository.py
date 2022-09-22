2cec2137b49eeba4e2147618d4a1f9d7562e3d0a
DuncanRN <knacknud@gmail.com>
2022-09-22 14:49:07 +0100

    classes and methods

--- repositories/author_repository.py
+++ repositories/author_repository.py
@@ -10,4 +10,38 @@ def save(author):
     id = results[0]['id']
     author.id=id
     return author
+
+def select(id):
+    author = None
+    sql = "SELECT * FROM authors WHERE id=%s"
+    values = [id]
+    results = run_sql(sql, values)
+
+    if results:
+        result = results[0]
+        author = Author(result['first_name'], result['last_name'], result['id'])
     
+    return author
+
+def select_all():
+    authors = []
+    sql = "SELECT * FROM authors"
+    results = run_sql(sql)
+    for row in results:
+        author = Author(row['first_name'], row['last_name'], row['id'])
+        authors.append(author)
+    return authors
+
+def update(author):
+    sql = "UPDATE authors SET (first_name, last_name) = (%s, %s) WHERE id = %s"
+    values = [author.first_name, author.last_name, author.id]
+    run_sql(sql, values)
+
+def delete(id):
+    sql = "DELETE FROM authors WHERE id= %s"
+    values =[id]
+    run_sql(sql, values)
+
+def delete_all():
+    sql = "DELETE FROM authors"
+    run_sql(sql)
